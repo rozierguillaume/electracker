aab1789049fd8ebdced34ecbc65d0f0eb46f72f7
guillaume.rozier <guillaume.rozier@nc-ass-vip.sdv.fr>
2021-06-04 18:00:08 +0200

    Autres regions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import urllib.request, json
 import pandas as pd
 
+REGIONS = ["PACA", "%20BZH", "ARA", "CVL", "GE", "HDF", "IDF", "N", "NA", "OCC", "PACA", "PDL"]
+
 def download_data(url):
     data = []
     with urllib.request.urlopen(url) as url_object:
@@ -36,9 +38,15 @@ def export_data(data, name):
         json.dump(data, outfile)
 
 
+def get_regions_polls():
+    for region in REGIONS:
+        name = f"regionales_{region}"
+        print(name)
+        data = download_data(f"https://raw.githubusercontent.com/nsppolls/nsppolls/master/{name}.json")
+        data_output = get_all_results(data)
+        data_output = compute_rolling_means(data_output)
+        export_data(data=data_output, name=name)
+
+
 if __name__ == '__main__':
-    name = "regionales_PACA"
-    data = download_data(f"https://raw.githubusercontent.com/nsppolls/nsppolls/master/{name}.json")
-    data_output = get_all_results(data)
-    data_output = compute_rolling_means(data_output)
-    export_data(data=data_output, name=name)
+    get_regions_polls()
