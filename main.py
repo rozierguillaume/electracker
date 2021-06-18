678e063ddae02ebf6d4b61b0eb57e00c6c0cf3cc
guillaume.rozier <guillaume.rozier@nc-ass-vip.sdv.fr>
2021-06-18 22:37:15 +0200

    gitignore

--- main.py
+++ main.py
@@ -53,7 +53,6 @@ def construire_liste_ordonnee_candidats(data):
     candidats = list(data.keys())
     intentions_rolling_mean = [round(data[candidat]["intentions_rolling_mean"][-1], 1) for candidat in candidats]
     indices_tries = np.argsort(-np.array(intentions_rolling_mean))
-    print(intentions_rolling_mean)
     return np.array(candidats)[indices_tries].tolist(), (np.array(intentions_rolling_mean)[indices_tries]).tolist()
 
 def get_all_results(data):
