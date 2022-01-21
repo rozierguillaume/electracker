1b4fe6c6679d2f19f1156252a85cb000c4b226c1
rozierguillaume <guillaume.rozier73@gmail.com>
2022-01-21 19:14:38 +0100

    update

--- src/elections_presidentielles_moyenne_mobile_intentions.py
+++ src/elections_presidentielles_moyenne_mobile_intentions.py
@@ -1,5 +1,6 @@
 import json 
 import pandas as pd
+import datetime
 
 df = pd.read_csv("https://raw.githubusercontent.com/nsppolls/nsppolls/master/presidentielle.csv")
 df = df[df["tour"] == "Premier tour"]
@@ -21,15 +22,18 @@ for candidat in CANDIDATS:
   df_temp = df[df["candidat"] == candidat]
   df_temp.index = pd.to_datetime(df_temp["fin_enquete"])
   
-  df_temp_rolling = round(df_temp[["intentions"]].rolling('14d', min_periods=1).mean().dropna(), 2)
-  df_temp_rolling_std = round(df_temp[["intentions"]].rolling('14d', min_periods=1).std().fillna(method="bfill"), 2)
+  df_temp_rolling = df_temp[["intentions"]].rolling('14d', min_periods=1).mean().dropna()
+  df_temp_rolling_std = df_temp[["intentions"]].rolling('14d', min_periods=1).std().fillna(method="bfill")
 
-  df_temp_rolling = df_temp_rolling.resample("1d").mean().dropna()
-  df_temp_rolling_std = df_temp_rolling_std.resample("1d").mean().dropna()
+  df_temp_rolling = round(df_temp_rolling.resample("1d").mean().dropna(), 2)
+  df_temp_rolling_std = round(df_temp_rolling_std.resample("1d").mean().dropna(), 2)
   
   dict_candidats[candidat] = {"intentions_moy_14d": {"fin_enquete": df_temp_rolling.index.strftime('%Y-%m-%d').to_list(), "valeur": df_temp_rolling.intentions.to_list(), "std": df_temp_rolling_std.intentions.to_list()},
                               "intentions": {"fin_enquete": df_temp.index.strftime('%Y-%m-%d').to_list(), "valeur": df_temp.intentions.to_list()},
-                              "couleur": CANDIDATS[candidat]["couleur"]}
+                              "couleur": CANDIDATS[candidat]["couleur"],}
+
+  dict_candidats["dernier_sondage"] = df["fin_enquete"].max()
+  dict_candidats["mise_a_jour"] = datetime.datetime.now().strftime(format="%Y-%m-%d %H:%M")
 
 with open('data/output/intentionsCandidatsMoyenneMobile14Jours.json', 'w') as outfile:
         json.dump(dict_candidats, outfile)
