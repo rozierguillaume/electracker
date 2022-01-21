1f1d13263085fbe6545b71edcf28454886928095
rozierguillaume <guillaume.rozier73@gmail.com>
2022-01-21 18:29:38 +0100

    WIP std

--- src/elections_presidentielles_moyenne_mobile_intentions.py
+++ src/elections_presidentielles_moyenne_mobile_intentions.py
@@ -22,8 +22,9 @@ for candidat in CANDIDATS:
   df_temp.index = pd.to_datetime(df_temp["fin_enquete"])
   df_temp_rolling = df_temp.resample("1d").mean()
   df_temp_rolling = round(df_temp[["intentions"]].rolling('14d', min_periods=1).mean().dropna(), 2)
+  df_temp_rolling_std = round(df_temp[["intentions"]].rolling('14d', min_periods=1).std().dropna(), 2)
   
-  dict_candidats[candidat] = {"intentions_moy_14d": {"fin_enquete": df_temp_rolling.index.strftime('%Y-%m-%d').to_list(), "valeur": df_temp_rolling.intentions.to_list()},
+  dict_candidats[candidat] = {"intentions_moy_14d": {"fin_enquete": df_temp_rolling.index.strftime('%Y-%m-%d').to_list(), "valeur": df_temp_rolling.intentions.to_list(), "std": df_temp_rolling_std.intentions.to_list()},
                               "intentions": {"fin_enquete": df_temp.index.strftime('%Y-%m-%d').to_list(), "valeur": df_temp.intentions.to_list()},
                               "couleur": CANDIDATS[candidat]["couleur"]}
 
