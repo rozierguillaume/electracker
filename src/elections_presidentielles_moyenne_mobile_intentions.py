1e1504a9ae59b3fd6125f1b2e43767d23669589d
rozierguillaume <guillaume.rozier73@gmail.com>
2022-01-22 15:31:23 +0100

    fix

--- src/elections_presidentielles_moyenne_mobile_intentions.py
+++ src/elections_presidentielles_moyenne_mobile_intentions.py
@@ -22,14 +22,18 @@ for candidat in CANDIDATS:
   df_temp = df[df["candidat"] == candidat]
   df_temp.index = pd.to_datetime(df_temp["fin_enquete"])
   
-  df_temp_rolling = df_temp[["intentions"]].rolling('14d', min_periods=1).mean().dropna()
+  df_temp_rolling = df_temp[["intentions", "erreur_inf", "erreur_sup"]].rolling('14d', min_periods=1).mean().dropna()
   df_temp_rolling_std = df_temp[["intentions"]].rolling('14d', min_periods=1).std().fillna(method="bfill")
 
   df_temp_rolling = round(df_temp_rolling.resample("1d").mean().dropna(), 2)
   df_temp_rolling_std = round(df_temp_rolling_std.resample("1d").mean().dropna(), 2)
+
+  #print(df_temp.columns)
+  #nom_institut, commanditaire, debut_enquete, fin_enquete, intentions
   
-  dict_candidats[candidat] = {"intentions_moy_14d": {"fin_enquete": df_temp_rolling.index.strftime('%Y-%m-%d').to_list(), "valeur": df_temp_rolling.intentions.to_list(), "std": df_temp_rolling_std.intentions.to_list()},
+  dict_candidats[candidat] = {"intentions_moy_14d": {"fin_enquete": df_temp_rolling.index.strftime('%Y-%m-%d').to_list(), "valeur": df_temp_rolling.intentions.to_list(), "std": df_temp_rolling_std.intentions.to_list(), "erreur_inf": df_temp_rolling.erreur_inf.to_list(), "erreur_sup": df_temp_rolling.erreur_sup.to_list()},
                               "intentions": {"fin_enquete": df_temp.index.strftime('%Y-%m-%d').to_list(), "valeur": df_temp.intentions.to_list()},
+                              "derniers_sondages": [],
                               "couleur": CANDIDATS[candidat]["couleur"]}
 
 dict_donnees = {"dernier_sondage": df["fin_enquete"].max(), 
